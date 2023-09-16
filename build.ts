82f3744d2c072b659de21a8d9b621af2d24e9df7
Der Pfandpirat <g.marquardt@teach-audio.com>
2023-09-16 17:41:15 +0200

    first update

---
+++ build.ts
@@ -0,0 +1,8 @@
+const path = require("path");
+const core = <reqSync_file>require("./core/shared/reqSync/RS_server.js");
+const {helpers} = core;
+
+core.analyse("./compiled",function(){
+    //const base = helpers.nodeJS.readFile("compiled/core/shared/reqSync/RS_client.js");
+    //helpers.parseJS?.completedFile("MVC");
+});
